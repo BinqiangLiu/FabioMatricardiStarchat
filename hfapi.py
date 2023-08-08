23fbcb5cf63a9a5f9e6e55d4791ba3f85f5bd408
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-08 23:09:56 +0800

    Update hfapi.py

--- hfapi.py
+++ hfapi.py
@@ -23,7 +23,7 @@ def starchat(model,myprompt, your_template):
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = yourHFtoken
     llm = HuggingFaceHub(repo_id=model,
                          model_kwargs={"min_length":100,
-                                       "max_new_tokens":1024, "do_sample":True,
+                                       "max_new_tokens":4096, "do_sample":True,
                                        "temperature":0.2,
                                        "top_k":50,
                                        "top_p":0.95, "eos_token_id":49155})
